598d4bf499f65ac3b52aac405a54db148a203d4c
joaobuga35 <joaobuga35@gmail.com>
2022-10-20 12:01:39 -0300

    styles about modals

---
+++ scripts/modals.js
@@ -0,0 +1,42 @@
+// <!-- <section class="modal-wrapper" id="modal-open-post">
+// <div class="div-modal">
+//     <div class="div-btn-profile-exit">
+//         <div class="profile-datas">
+//             <img class="img-post" src="/assets/img/Ellipse 1.svg" alt="">
+//             <span class="name-profile">Samuel Leão</span>
+//             <span class="date-profile">| Outubro 2022</span>
+//         </div>
+
+//         <div class="exit-modal">
+//             <button class="btn-exit-modal" id="btn-exit-modal-post">X</button>
+//         </div>
+//     </div>
+
+//     <h2>Outubro Rosa: Detalhes sobre a importância da prevenção do câncer de mama em cadelas e gatas</h2>
+//     <p>Assim como em humanos, cadelas e gatas também podem desenvolver câncer de mama. Ainda hoje, para ambas as espécies, o câncer de mama tem maior incidência. Mesmo com a evolução da medicina veterinária e da oncologia, o câncer de mama muitas vezes não tem cura, sendo o tratamento paliativo uma alternativa para dar conforto às fêmeas. 
+
+//         Por isso, a conscientização sobre o tema é uma das ações de prevenção de maior importância, principalmente para a campanha do outubro rosa. Então como podemos ajudar a prevenir o câncer de mama em cadelas e gatas?
+        
+//         Assim como outros tipos de câncer, o câncer de mama pode ter inúmeras causas. Entre  as mais comuns, temos fatores genéticos, como predisposição de algumas raças, exposição à poluição, tabagismo passivo, obesidade, além do fato de os pets estarem vivendo mais, o que também pode causar maior chances de tumores. No caso do câncer de mama, a influência hormonal é um dos fatores de maior contribuição para o aparecimento dessa doença.
+//     </p>
+// <div>                           
+// </section> -->
+
+{/* <section class="modal-wrapper">
+    <div class="div-delete-post">
+        <div class="div-btn-profile-exit">
+            <h3>Confirmação de exclusão</h3>
+            <button class="btn-exit-modal">X</button>
+        </div>
+
+        <div class="description-delete">
+            <h2>Tem certeza que deseja excluir este post?</h2>
+            <p>Essa ação não poderá ser desfeita, então pedimos que tenha cautela antes de concluir</p>
+        </div>
+
+        <div class="div-btn-cancel-delete">
+            <button id="cancel-delete" class="btn-cancel-delete">Cancelar</button>
+            <button id="button-delete-post" class="btn-delete-post ">Sim,excluir este post</button>
+        </div>
+    </div>
+</section> */}
